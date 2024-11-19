c2ef2a02da5945835a43decb799dcf0f28638156
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2024-11-19 14:36:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 3000,
+//   }
+// })
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000,
-  }
-})
+    proxy: {
+      '/transcribe': {
+        target: 'https://mediscribeai-backend.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
